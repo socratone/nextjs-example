92d653de1a633da20ce45149ef65a4202968df47
socratone <gim2origin@gmail.com>
2021-12-18 02:16:32 +0900

    feat: SPA 방식의 Link 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>hello world</h1>
+      <Link href="/basic">Basic</Link>
     </div>
   );
 };
