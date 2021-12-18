a520f541e15f5ac31f84f4d93eb900339930f1fb
socratone <gim2origin@gmail.com>
2021-12-18 18:30:42 +0900

    feat: router push 사용 예제 추가

--- pages/basic/index.tsx
+++ pages/basic/index.tsx
@@ -1,5 +1,26 @@
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 const Basic = () => {
-  return <h1>Basic</h1>;
+  const router = useRouter();
+
+  const handleClick = (id: string) => {
+    router.push('/basic/' + id);
+  };
+
+  return (
+    <>
+      <h1>BASIC</h1>
+      <ul>
+        <Item onClick={() => handleClick('1')}>Basic 1</Item>
+        <Item onClick={() => handleClick('2')}>Basic 2</Item>
+      </ul>
+    </>
+  );
 };
+
+const Item = styled.li`
+  cursor: pointer;
+`;
 
 export default Basic;
