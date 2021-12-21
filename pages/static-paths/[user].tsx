b42abb68c3482469d474106b8d292666f73fa8d8
socratone <gim2origin@gmail.com>
2021-12-21 21:40:09 +0900

    fix: static paths의 fallback 설정 설명 추가

--- pages/static-paths/[user].tsx
+++ pages/static-paths/[user].tsx
@@ -12,6 +12,8 @@ const User = ({ user }: UserProps) => {
 export const getStaticPaths: GetStaticPaths = () => {
   // fallback을 false로 설정하면 user에 해당하는 route에 john과 peter만 가능하고 나머지는 404가 된다.
   return {
+    // 404 페이지 대신 데이터 없는 빈 페이지가 나오게 하려면 true 또는 'blocking'으로 설정할 수 있다.
+    // 'blocking'은 데이터를 받아올 때까지 아무것도 보여주지 않는다.
     fallback: false,
     paths: [
       {
