c4caa002e764aa259c65054225794bdb40212d3f
John Kim <john@gymtinc.com>
2022-01-01 16:44:51 +0900

    fix: serverless function deploy에 관한 주석 작성

--- pages/api/todo.ts
+++ pages/api/todo.ts
@@ -1,3 +1,9 @@
+/**
+ * /pages/api에 아래처럼 function을 생성하고 vercel에 연결한 repository에 프로젝트 코드를 push하면
+ * 별다른 config 없이도 알아서 Serverless Function을 deploy하게 된다.
+ * https://vercel.com/docs/concepts/functions/serverless-functions#deploying-serverless-functions
+ */
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
