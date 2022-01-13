abdd0c6868f32130b11696bd333b7c2e278bc249
socratone <gim2origin@gmail.com>
2022-01-13 23:39:05 +0900

    fix: getStaticPaths 함수에 대해 자세한 설명 추가

--- pages/static-paths/[user].tsx
+++ pages/static-paths/[user].tsx
@@ -8,12 +8,16 @@ const User = ({ user }: UserProps) => {
   return <h1>{user}</h1>;
 };
 
-// 다이나믹 라우트를 쓸 때 모든 라우트에 해당하는 페이지를 다 생성하지 않도록 설정 하기 위해서 다음 함수를 쓴다.
+// 다이나믹 라우트에 getStaticProps 함수를 쓰면 여기서 나오는 모든 페이지를 미리 렌더링 하겠다는 의미다.
+// 그러나 무한대 경우의 수만큼 미리 렌더링을 할 수는 없기 때문에 getStaticPaths 함수를 써서 범위를 한정한다.
 export const getStaticPaths: GetStaticPaths = () => {
   // fallback을 false로 설정하면 user에 해당하는 route에 john과 peter만 가능하고 나머지는 404가 된다.
   return {
     // 404 페이지 대신 데이터 없는 빈 페이지가 나오게 하려면 true 또는 'blocking'으로 설정할 수 있다.
-    // 'blocking'은 데이터를 받아올 때까지 아무것도 보여주지 않는다.
+    // paths에 포함된 경로만 미리 렌더링을 하고 나머지는 요청이 있을 때 렌더링한다는 의미다.
+    // 빌드시 엄청나게 많은 페이지를 렌더링 해야하는 경우 리소스 낭비이기 때문에 자주 접속할 페이지만 렌더링 하도록 paths에 포함시킨다.
+    // true는 데이터를 받아오기 전에 렌더링하고 'blocking'은 데이터를 받아오고 난 뒤에 렌더링을 한다.
+    // 페이지를 빨리 보여줄 것인지 완벽한 페이지를 보여줄 것인지에 따라서 둘 중 하나를 선택한다.
     fallback: false,
     paths: [
       {
