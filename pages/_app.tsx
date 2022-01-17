ad4679f171ecd665eccace15f1c375779bb4b501
socratone <gim2origin@gmail.com>
2022-01-17 23:00:36 +0900

    fix: 공통 meta 태그 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // Layout을 모든 페이지에 적용한다.
     <Layout>
+      <Head>
+        {/* 아래 meta 태그는 개별적으로 설정한 태그와 중복된 채 모든 페이지에 적용된다. */}
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
