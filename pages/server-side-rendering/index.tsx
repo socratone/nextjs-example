2d687e9bd9e129760f075b1037b643bc3b028b90
socratone <gim2origin@gmail.com>
2022-03-19 14:52:48 +0900

    fix: getServerSideProps 함수 주석 수

--- pages/server-side-rendering/index.tsx
+++ pages/server-side-rendering/index.tsx
@@ -12,6 +12,8 @@ const ServerSideRendering = ({ data }: ServerSideRenderingProps) => {
 };
 
 // getServerSideProps라는 함수가 있으면 함수 내부에서 서버 데이터를 가져온 뒤 빌드한다.
+// pre-generated되지 않고 요청을 할 때마다 빌드한다.
+// 때문에 getStaticPaths를 쓸 필요가 없다.
 export const getServerSideProps = async () => {
   // fetch
   return {
