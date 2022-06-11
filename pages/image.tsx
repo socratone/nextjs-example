21195d99acf3a4f792b072218730157f77abee47
Kiwon Kim <kiwon@elicer.com>
2022-06-11 23:55:01 +0900

    feat: image layout 예제 추가

--- pages/image.tsx
+++ pages/image.tsx
@@ -7,12 +7,75 @@ const ImagePage = () => {
   return (
     <>
       <h1>Image</h1>
+
+      <h2>{'layout="intrinsic"'}</h2>
       <Image
         src="/images/developer.jpg"
         alt="developer"
         width="640"
         height="427"
       />
+
+      <h2>{'layout="fill"'}</h2>
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '300px',
+          border: '1px solid tomato',
+        }}
+      >
+        <Image src="/images/developer.jpg" alt="developer" layout="fill" />
+      </div>
+
+      <h2>{'layout="fill" objectFit="contain"'}</h2>
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '300px',
+          border: '1px solid tomato',
+        }}
+      >
+        <Image
+          src="/images/developer.jpg"
+          alt="developer"
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+
+      <h2>{'layout="fill" objectFit="cover"'}</h2>
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '300px',
+          border: '1px solid tomato',
+        }}
+      >
+        <Image
+          src="/images/developer.jpg"
+          alt="developer"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
+      <h2>{'layout="responsive"'}</h2>
+      <div
+        style={{
+          border: '1px solid tomato',
+        }}
+      >
+        <Image
+          src="/images/developer.jpg"
+          alt="developer"
+          width="640"
+          height="427"
+          layout="responsive"
+        />
+      </div>
     </>
   );
 };
